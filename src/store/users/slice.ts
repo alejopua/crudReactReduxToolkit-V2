ae01dee8bf238e1469ab440fb5b714d71e5ea4ad
alejopua <alejopua@gmail.com>
2024-04-22 18:10:07 -0500

    Add users slice to manage user state in the store

---
+++ src/store/users/slice.ts
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export interface User {
+	name: string
+	role: string
+	email: string
+	github: string
+}
+
+export const usersSlice = createSlice({
+	name: 'users',
+	initialState: {},
+	reducers: {},
+})
