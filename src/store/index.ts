3080299ff381425711e334d58fdb0f1d6ad4cfc9
alejopua <alejopua@gmail.com>
2024-04-23 13:04:06 -0500

    Refactor usersSlice in src/store/users/slice.ts to include deleteUser action and handle initial state from local storage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { configureStore } from '@reduxjs/toolkit'
 import usersReducer from './users/slice'
+
+const localStoragePersistanceMiddleware =
+	(store: { getState: () => any }) =>
+	(next: (arg0: any) => void) =>
+	(action: any) => {
+		next(action)
+		localStorage.setItem('__redux_state__', JSON.stringify(store.getState()))
+	}
 
 export const store = configureStore({
 	reducer: {
 		users: usersReducer,
 	},
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware().concat(localStoragePersistanceMiddleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
