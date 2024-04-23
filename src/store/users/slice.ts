b64ab823123ece06c0a5d6757f07ac7c4340ed41
alejopua <alejopua@gmail.com>
2024-04-22 19:01:01 -0500

    Refactor users slice to include UserId interface and initial user data

--- src/store/users/slice.ts
+++ src/store/users/slice.ts
@@ -7,8 +7,45 @@ export interface User {
 	github: string
 }
 
+export interface UserId extends User {
+	id: string
+}
+
+const initialState: UserId[] = [
+	{
+		id: '1',
+		name: 'Jane Cooper',
+		role: 'Admin',
+		email: 'jane.cooper@example.com',
+		github: 'johndoe',
+	},
+	{
+		id: '2',
+		name: 'John Doe',
+		role: 'Tester',
+		email: 'john.doe@example.com',
+		github: 'johndoe',
+	},
+	{
+		id: '3',
+		name: 'Veronica Lodge',
+		role: ' Software Engineer',
+		email: 'veronica.lodge@example.com',
+		github: 'veronicalodge',
+	},
+	{
+		id: '4',
+		name: 'Peter  Parker',
+		role: 'Software Movile',
+		email: 'peter@example.com',
+		github: 'alejopua',
+	},
+]
+
 export const usersSlice = createSlice({
 	name: 'users',
-	initialState: {},
+	initialState,
 	reducers: {},
 })
+
+export default usersSlice.reducer
