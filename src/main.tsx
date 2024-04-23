d3114da423f50fe3d40424cb72364fd1b86bae20
alejopua <alejopua@gmail.com>
2024-04-23 11:49:00 -0500

    Refactor import statement in main.tsx to use store from the correct location

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { store } from './store/index'
+
+import { store } from './store'
 import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
