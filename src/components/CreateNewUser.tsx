c10cfe56b4e9ce307540eba023c5d652a980d16e
alejopua <alejopua@gmail.com>
2024-04-24 10:23:36 -0500

    Refactor CreateNewUser component to include placeholder text for input fields in src/components/CreateNewUser.tsx

--- src/components/CreateNewUser.tsx
+++ src/components/CreateNewUser.tsx
@@ -47,54 +47,39 @@ export const CreateNewUser = () => {
 					</div>
 					<div className='m-8 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
 						<div className='sm:col-span-2 sm:col-start-1'>
-							<label
-								htmlFor='name'
-								className='block text-sm font-medium leading-6 text-gray-900'
-							>
-								Name
-							</label>
 							<div className='mt-2'>
 								<input
 									type='text'
 									name='name'
 									id='name'
 									autoComplete='name'
+									placeholder='name'
 									className='block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
 								/>
 							</div>
 						</div>
 
 						<div className='sm:col-span-2'>
-							<label
-								htmlFor='email'
-								className='block text-sm font-medium leading-6 text-gray-900'
-							>
-								Email
-							</label>
 							<div className='mt-2'>
 								<input
 									type='email'
 									name='email'
 									id='email'
 									autoComplete='email'
+									placeholder='email'
 									className='block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
 								/>
 							</div>
 						</div>
 
 						<div className='sm:col-span-2'>
-							<label
-								htmlFor='role'
-								className='block text-sm font-medium leading-6 text-gray-900'
-							>
-								Role
-							</label>
 							<div className='mt-2'>
 								<input
 									type='text'
 									name='role'
 									id='role'
 									autoComplete='role'
+									placeholder='role'
 									className='block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
 								/>
 							</div>
