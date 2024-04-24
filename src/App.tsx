ae53e7eed3691fddbbed7791a183e5e09eb3f686
alejopua <alejopua@gmail.com>
2024-04-24 00:57:24 -0500

    Refactor App component to include CreateNewUser and ListOfUsers components in src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { CreateNewUser } from './components/CreateNewUser'
 import ListOfUsers from './components/ListOfUsers'
 
 function App() {
 	return (
 		<>
-			<ListOfUsers />
+			<div className='box-border bg-slate-700 p-3 '>
+				<div className='flex flex-col min-w-full sm:{px-6 -mx-6} lg:{px-8 -mx-8} '>
+					<CreateNewUser />
+					<ListOfUsers />
+				</div>
+			</div>
 		</>
 	)
 }
