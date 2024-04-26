c5c751a92ab0c1f27a7147d69b479a1e7f5a9867
alejopua <alejopua@gmail.com>
2024-04-26 00:33:39 -0500

    Add EditUser modal component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { CreateNewUser } from './components/CreateNewUser'
 import ListOfUsers from './components/ListOfUsers'
+import { CreateNewUser } from './components/CreateNewUser'
+import { EditUser } from './components/modal/EditUser'
+import { useAppSelector } from './hooks'
 
 function App() {
+	const { isOpen } = useAppSelector(state => state.modal)
 	return (
 		<>
 			<div className='h-screen box-border bg-slate-700 p-3 '>
 				<div className='h-full flex flex-col min-w-full sm:{px-6 -mx-6} lg:{px-8 -mx-8} '>
 					<CreateNewUser />
 					<ListOfUsers />
+					{isOpen && <EditUser />}
 				</div>
 			</div>
 		</>
